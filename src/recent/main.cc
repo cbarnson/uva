7c5bc9b7ef673715a94f59075537c7eccbe30c7f
Cody Barnson <cbarnson@outlook.com>
2018-05-02 01:53:23 -0600

    test commit, adding recent folder

---
+++ src/recent/main.cc
@@ -0,0 +1,43 @@
+// Problem #    :
+// Title        :
+// Accepted     : No
+// Date         : 20180501
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <algorithm>
+#include <bitset>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+typedef vector<vector<int>> vec2d;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << n;
+
+  return 0;
+}
