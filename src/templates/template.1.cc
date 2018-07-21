500314868adcc453003d0e39a588625026789b48
Cody Barnson <cbarnson@outlook.com>
2018-07-21 14:01:11 -0600

    update

--- src/templates/template.1.cc
+++ src/templates/template.1.cc
@@ -26,6 +26,8 @@
 
 using namespace std;
 
+#define sz(a) (int)a.size()
+
 typedef pair<int, int> ii; // pair of ints
 typedef vector<int> vi;    // 1d vector of ints
 typedef vector<ii> vii;    // 1d vector of pairs
@@ -33,6 +35,21 @@ typedef vector<vi> vvi;    // 2d vector of ints
 typedef vector<vii> vvii;  // 2d vector of pairs
 typedef vector<bool> vb;   // 1d vector of bools
 typedef long long ll;
+
+ostream &operator<<(ostream &os, ii &p) {
+  os << "(" << p.first << ", " << p.second << ")\n";
+}
+
+ostream &operator<<(ostream &os, vi &v) {
+  for (auto &i : v)
+    os << i << " ";
+  os << endl;
+}
+ostream &operator<<(ostream &os, vector<char> &v) {
+  for (auto &i : v)
+    os << i << " ";
+  os << endl;
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
