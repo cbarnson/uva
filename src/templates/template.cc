8ef26a3e391881b4914645c599d00159b3e53aca
Cody Barnson <cbarnson@outlook.com>
2018-08-06 04:57:33 -0600

    gitignore update

--- src/templates/template.cc
+++ src/templates/template.cc
@@ -2,26 +2,22 @@
 #include <bits/stdc++.h>
 
 #define REP(i, n) for (int i = 0; i < (int)n; ++i)
-#define FOR(i, c) \
+#define FOR(i, c)                                                              \
   for (__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
 #define ALL(c) (c).begin(), (c).end()
-#define MP make_pair
-#define L first
-#define R second
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
 
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, int> ii;  // pair of ints
-typedef vector<int> vi;     // 1d vector of ints
-typedef vector<ii> vii;     // 1d vector of pairs
+typedef pair<int, int> ii; // pair of ints
+typedef vector<int> vi;    // 1d vector of ints
+typedef vector<ii> vii;    // 1d vector of pairs
+typedef vector<vi> vvi;    // 2d vector of ints
 
 int main() {
-  // Uncomment to use c++ IO (cout/cin) faster,
-  // but don't use scanf/printf if you do.
-  // ios_base::sync_with_stdio(false);
-  // cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-  
   return 0;
 }
