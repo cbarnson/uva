01cf8beaf8561a446ec9cbc61689f750c022a9a8
Cody Barnson <cbarnson@outlook.com>
2018-09-14 15:32:53 -0600

    finished solns for cf week 1 set

--- src/codeforces/228444/Y/Y.cc
+++ src/codeforces/228444/Y/Y.cc
@@ -5,10 +5,26 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
+int toMin(ii hm) {
+   return hm.first * 60 + hm.second;
+}
+
+ii toHM(int m) {
+   return ii(m / 60, m % 60);
+}
+
 int main() {
    
+   int hh, mm, H, D, C, N;
+   double tc, tcd;
+   cin >> hh >> mm >> H >> D >> C >> N;
 
+   tc = C * ((H / N) + (H % N ? 1 : 0));
 
+   int H2 = H + (D * (max(0, toMin(ii(20, 0)) - toMin(ii(hh, mm)))));
+   tcd = (double)C * 0.8 * (double)((H2 / N) + (H2 % N ? 1 : 0));
+
+   cout << setprecision(4) << fixed << min(tc, tcd) << endl;
 
 }
 
--- src/codeforces/228444/Z/Z.cc
+++ src/codeforces/228444/Z/Z.cc
@@ -7,7 +7,15 @@ typedef pair<int, int> ii;
 
 int main() {
    
+   int n;
+   string s, r = "";
+   cin >> n >> s;
 
+   for (int i = 1; i < s.length(); i++) 
+      if (s[i] != s[i-1] && s[i-1] != 'D') 
+	 s[i] = 'D';
+
+   cout << ((int)s.length() - count(s.begin(), s.end(), 'D')) << endl;
 
 
 }
