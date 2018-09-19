476d5eeac761852a7b9f124eb56dc69bf7742d1a
Cody Barnson <cbarnson@outlook.com>
2018-09-19 09:22:57 -0600

    update with rmq template

---
+++ drill/main.cc
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+
+class RMQ {
+public:
+   vector<int> A;
+   vector< vector<int> > M;
+   RMQ(const vector<int> &_A) {
+      A = _A;
+      int n = A.size(), m = 31 - __builtin_clz(n) + 1;
+
+      M.assign(m, vector<int>(n));
+      rep(j, n) M[0][j] = j;
+
+      for (int i = 1; (1 << i) <= n; i++) {
+	 for (int j = 0; (j + (1 << i)) <= n; j++) {
+	    M[i][j] = (A[M[i-1][j]] <= A[M[i-1][j + (1 << (i - 1))]])
+	       ? M[i-1][j]
+	       : M[i-1][j + (1 << (i - 1))];
+	 }
+      }
+   }
+
+   int query(int L, int R) {
+      int k = 31 - __builtin_clz(R - L + 1);
+      return (A[M[k][L]] <= A[M[k][R - (1 << k) + 1]])
+	 ? M[k][L]
+	 : M[k][R - (1 << k) + 1];
+   }
+
+};
+
+int main() {
+
+   int n;
+   cin >> n;
+
+   vector<int> A(n);
+   for (auto &i : A) cin >> i;
+
+   RMQ X(A);   
+
+   vector<int> ans(n);
+   for (int L = 0; L < n; L++) {
+      int skip = 1;
+      if (n > 10000) skip = (rand() & 15) + 1;
+      for (int R = L; R < n; R += skip) {
+	 ans[L] += (ans[L] >> 5) ^ (ans[L] << 4) ^ X.query(L, R);
+      }
+   }
+   for (int i = 0; i < n; i++) {
+      cout << (i ? " " : "") << ans[i];
+   }
+   cout << endl;
+
+}
