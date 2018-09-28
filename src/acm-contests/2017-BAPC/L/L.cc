713572962c378e88f9f08445e042a824ad8c4317
Cody Barnson <cbarnson@outlook.com>
2018-09-28 11:59:11 -0600

    soln for L of 2017 BAPC

--- src/acm-contests/2017-BAPC/L/L.cc
+++ src/acm-contests/2017-BAPC/L/L.cc
@@ -11,6 +11,26 @@ int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
 
+   int n;
+   cin >> n;
 
+   // 0.5 < r < 2, log(r) can be negative, use large neg inf 
+   map<string, double> MP = { { "pink", log(1.0) }, { "blue", 0x3f3f3f3f * -1.0 } };
+   
+   FR(i, n) {
+
+     string u, v; cin >> u >> v;
+     double r; cin >> r;
+     
+     // add if either not seen before
+     for (auto s : {u, v}) {
+       if (MP.find(s) == MP.end())
+	 MP.insert({s, 0x3f3f3f3f * -1.0 });
+     }
+     MP[u] = max(MP[u], MP[v] + log(r));
+   }
+
+   cout << setprecision(6) << fixed << min(10.0, exp(MP["blue"])) << endl;
+   
 }
    
