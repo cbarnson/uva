03a5a8ec54b4d0f06fc462bdcf6c4b2906f94998
Cody Barnson <cbarnson@outlook.com>
2018-09-28 08:51:58 -0600

    pre merge

--- src/codeforces/228874/E/E.cc
+++ src/codeforces/228874/E/E.cc
@@ -6,27 +6,11 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
-const int NN = 200001;
-
-int C[NN], D[NN], K[NN];
+vi C, K;
 vector<vi> g;
-
-int centroid(int s, int k) {
-   int u = s, e = s;
-   while (1) {
-      int x = k - C[u], y = -1, yv = -1;
-      for (auto &v : g[u])
-	 if (v != e && C[v] >= y)
-	    y = C[v], yv = v;
-      if (max(x, y) <= k/2) break;
-      e = u, u = yv;
-   }
-   return u;
-}
 
 void dfs(int u, int e) {
    C[u] = K[u];
-   D[u] = D[e] + (u != e);
    for (auto &v : g[u])
       if (v != e) 
 	 dfs(v, u), C[u] += C[v];      
@@ -39,37 +23,26 @@ int main() {
    int n, k, u, v;
    cin >> n >> k;
 
-   FR(i, 2*k) {
-      int x; cin >> x; K[x-1] = 1;
-   }
-
    g.assign(n, vi());
+   C.assign(n, 0);
+   K.assign(n, 0);
+
+   FR(i, 2*k) { cin >> u; u--, K[u] = 1; }
    FR(i, n-1) {
-      cin >> u >> v; u--, v--;
-      g[u].push_back(v); g[v].push_back(u);
+     cin >> u >> v; u--, v--;
+     g[u].push_back(v); g[v].push_back(u);
    }
 
+   // get subtree sizes
    dfs(0, 0);
-   int c = centroid(0, 2*k);
-   cout << "c " << c << endl;
-   
-   fill_n(C, n, 0);
-   fill_n(D, n, 0);
-   dfs(c, c);
 
+   // edge counted as many times as there are connections
+   // betw node in subtree and some node not in subtree
+   ll s = 0;
    FR(i, n) {
-      cout << D[i] << "-";
+     s += min(C[i], 2*k - C[i]);
    }
-   cout << endl;
-
-   FR(i, n) {
-      cout << C[i] << "-";
-   }
-   cout << endl;
-
-   int s = 0;
-   FR(i, n) s += (K[i] ? D[i] : 0);
-   cout << s << endl;    
+   cout << s << endl;
 }
 
 
--- src/codeforces/228874/H/H.cc
+++ src/codeforces/228874/H/H.cc
@@ -6,12 +6,48 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
+int C[2][1005];
+
 int main() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
+  int n, k, x;
+  cin >> n >> k >> x;
 
+  FR(i, n) {
+    int y; cin >> y;
+    C[0][y]++;
+  }
 
+  for (int i = 0; i < k; i++) {
+    int cnt = 0, p = i & 1;
+
+    for (int j = 0; j <= 1000; j++) {
+      for (int l = 0; l < C[p][j]; l++) {
+	cout << j << " ";
+      }
+      cout << endl;
+    }
+
+    
+    for (int j = 0; j <= 1000; j++) {
+      while (C[p][j]) {
+	C[p][j]--, cnt++;
+	if (cnt & 1)
+	  C[!p][j^x]++;
+	else
+	  C[!p][j]++;
+      }
+    }
+  }
+   
+  int i = 0, j = n - 1, p = k & 1;
+  while (!C[p][i]) i++;
+  while (!C[p][j]) j--;
+
+  cout << j << " " << i << endl;
+  
 }
 
 
