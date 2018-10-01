8e20466ed34ab9b5d62f9ec3efa18eaee291b859
Cody Barnson <cbarnson@outlook.com>
2018-09-30 18:44:30 -0600

    wip selection1 A

--- selection-contest-1/A/A.cc
+++ selection-contest-1/A/A.cc
@@ -6,8 +6,27 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<ll, ll> ii;
 
-int lcm(int a, int b) {
-   return a * (b / __gcd(a, b));
+vector<ll> fac(ll n) {
+   if (n < 2) return vector<ll>();
+
+   vector<ll> ans;
+   if (n % 2 == 0) {
+      ans.push_back(2);
+      while (n % 2 == 0) n /= 2;
+   }
+
+   for (ll i = 3; i * i <= n; i += 2) {
+      if (n % i == 0) {
+	 ans.push_back(i);
+	 while (n % i == 0) n /= i;
+      }
+   }
+
+   if (n > 1)
+      ans.push_back(n);
+
+   return ans;
+   
 }
 
 int main() {
@@ -15,38 +34,31 @@ int main() {
    ios_base::sync_with_stdio(false);
    cin.tie(NULL);
 
-   vector<ll> P = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-   
    int n;
    cin >> n;
 
-   vector<ii> A(n);
+   
+   
+   vector<ii> A(n, ii(0, 0));
    for (auto &x : A) cin >> x.first >> x.second;
+   vector<ll> f = fac(A[0].first * A[0].second);
 
-   if (n == 1) {
-      cout << A[0].first << endl;
-      return 0;
-   }
-
-   vi cp(P.size(), 0);
-   for (int i = 0; i < n; i++) {
-      ll ab = A[i].first * A[i].second;      
-      for (int j = 0; j < P.size(); j++) {
-	 if (__gcd(ab, P[j]) == 1)
-	    cp[j] = 1;
+   // try each factor, until find ans or run out of factors
+   for (int i = 0; i < (int)f.size(); i++) {
+      ll x = f[i];
+      int good = 1;
+      for (auto &y : A) {
+	 if (__gcd(x, y.first) == 1 && __gcd(x, y.second) == 1) {
+	    good = 0; break;
+	 }
       }
-   }
 
-   for (int j = 0; j < P.size(); j++) {
-      if (cp[j] == 0) {
-	 cout << P[j] << endl;
+      if (good) {
+	 cout << x << endl;
 	 return 0;
       }
    }
 
    cout << -1 << endl;
-
    
-   
-
 }
