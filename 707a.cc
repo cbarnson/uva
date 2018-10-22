cdd706ec80f30cd86a1d781821a6dc49e75f2292
Cody Barnson <cbarnson@outlook.com>
2018-10-21 18:06:06 -0600

    wip

---
+++ 707a.cc
@@ -0,0 +1,20 @@
+// Problem #    : 707a
+// Created on   : 2018-10-21 17:07:28
+#include <bits/stdc++.h>
+#define FR(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii; 
+typedef vector<int> vi;    
+
+int main() {
+
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   
+   
+
+
+}
