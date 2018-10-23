54dd10e8240a820033e502c1893d366dea4280ea
Cody Barnson <cbarnson@outlook.com>
2018-10-23 14:49:06 -0600

    update

---
+++ 149a.cc
@@ -0,0 +1,20 @@
+// Problem #    : 149a
+// Created on   : 2018-10-23 14:15:40
+#include <bits/stdc++.h>
+#define FR(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii; 
+typedef vector<int> vi;    
+
+int main() {
+
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   
+   
+
+
+}
--- mylib/snip.cc
+++ mylib/snip.cc
@@ -167,6 +167,65 @@ void LIS_n2_desc(int n) {
    return desc[0];
 }
 
+
+
+
+
+
+
+
+
+// Maximal Matching (Hopcroft-Karp)
+// https://en.wikipedia.org/wiki/Hopcroft–Karp_algorithm
+// time complexity: O(E * sqrt(V))
+class MaxMatching {
+public:
+   static tuple<int, vi> max_matching(const vector<vi> &g) {
+      int m = 0, n = g.size();
+    
+      for (auto &gg : g) for (int u : gg) m = max(m, u + 1);
+      vi A(m, -1), D(n), used(n);      
+
+      for (int i = 0, f = 0;; i += f, f = 0) {
+      	 vi vis(n); 
+      	 bfs(g, used, A, D);
+      	 FR(u, n) if (!used[u] && dfs(g, vis, used, A, D, u)) f++;
+      	 if (!f) return make_tuple(i, A);
+      }
+   }
+
+   static void bfs(const vector<vi> &g, vi &used, vi &A, vi &D) {
+      int n = g.size(), q = 0;
+      fill(begin(D), end(D), -1);
+      vi Q(n);
+      FR(u, n) if (!used[u]) Q[q++] = u, D[u] = 0;
+      FR(i, q) {
+	 int u = Q[i];
+	 for (int v : g[u]) {
+	    int w = A[v];
+	    if (w >= 0 && D[w] < 0) D[w] = D[u] + 1, Q[q++] = w;
+	 }
+      }
+   }
+
+   static bool dfs(const vector<vi> &g,
+		   vi &vis, vi &used,
+		   vi &match, vi &D, int u) {
+      vis[u] = 1;
+      for (int v : g[u]) {
+	 int w = match[v];
+	 if (w < 0 || (!vis[w] && D[w] == D[u] + 1 &&
+		       dfs(g, vis, used, match, D, w))) {
+	    match[v] = u;
+	    used[u] = true;
+	    return true;
+	 }
+      }
+      return false;
+   }
+};
+
+
 // Modular Exponentiation
 // Compute x^n mod m
 int modexp(int x, int n, int m) {
