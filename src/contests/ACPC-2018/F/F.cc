0491eb34d262529df0ac34f9ca4c50ed02106a23
Cody Barnson <cbarnson@outlook.com>
2018-11-01 03:27:08 -0700

    solution for ACPC problem F

--- src/contests/ACPC-2018/F/F.cc
+++ src/contests/ACPC-2018/F/F.cc
@@ -6,11 +6,35 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> ii;
 
+// time intersect
+double ti(double a0, double b0, double va, double vb) {
+  return ((a0 - b0) / (va - vb));
+}
+
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 
-   ios_base::sync_with_stdio(false);
-   cin.tie(0);
+  cout << fixed << setprecision(10);
 
+  // --- (0) ----------------- < ===== a0 ===== >
+  //                           < ===== b0 ===== >
 
+  double ax, ay, va, ri, rf, bx, by, vb;
+  cin >> ax >> ay >> va >> ri >> rf >> bx >> by >> vb;
 
+  double b0 = hypot(bx - ax, by - ay), a0 = ri;
+  double tb = (b0 - rf) / vb;
+
+  double dmg = 0.0;
+  if (b0 < a0 && va > vb) {
+    double i = ti(a0, b0, va, vb);
+    if (i < tb) dmg = (tb - i);
+  } else if ((b0 == a0 && va > vb) || (b0 > a0 && va >= vb)) {
+    dmg = tb;
+  } else if (b0 > a0 && va < vb) {
+    double i = ti(a0, b0, va, vb);
+    dmg = (i < tb) ? i : tb;
+  }
+  cout << dmg << endl;
 }
