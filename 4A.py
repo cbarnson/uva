4a30e8c5e70bc677e45beb5ef0fd6a40c209fbe2
Cody Barnson <cbarnson@outlook.com>
2019-01-12 00:19:00 -0700

    update

--- 4A.py
+++ 4A.py
@@ -1,12 +1,16 @@
+#! python
+
 # Problem #    : 4A
 # Created on   : 2019-01-11 22:42:54
 
+
 def Main():
-  x = int(input())
-  if x > 1:
-    y = 2
-    x -= 2
-    print('NO' if x & 1 else 'YES')
+    x = int(input())
+    if x > 1:
+        y = 2
+        x -= 2
+        print('NO' if x & 1 else 'YES')
+    print('NO')
 
 if __name__ == '__main__':
-  Main()
+    Main()
