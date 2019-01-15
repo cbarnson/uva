d008583486eac6e1d062fcab7a997a7ef20d5f2c
Cody Barnson <cbarnson@outlook.com>
2019-01-14 22:14:10 -0700

    update

---
+++ 263A.py
@@ -0,0 +1,12 @@
+#! python
+
+# Problem #    : 263A
+# Created on   : 2019-01-14 22:13:53
+
+
+def Main():
+    pass
+
+
+if __name__ == '__main__':
+    Main()
--- default_cf.py
+++ default_cf.py
@@ -1,6 +1,8 @@
 
+
 def Main():
     pass
+
 
 if __name__ == '__main__':
     Main()
